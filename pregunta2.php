c8893945ff69bbe93e1687dc5acb9b30bddab6ed
Nelvi975 <73485821+Nelvi975@users.noreply.github.com>
2020-11-23 09:02:17 -0500

    Add files via upload

---
+++ pregunta2.php
@@ -0,0 +1,27 @@
+<?php
+//Saber si un numero es primo. 7
+$np=25;
+ 
+if(primo($np)){
+echo " el numero $np es primo";
+}
+else {
+	echo " el numero $np es compuesto";
+}
+
+function primo($num){
+if($num == 1 || $num == 2 || $num == 3 || $num == 5 || $num == 7){
+	return true;
+}
+if($num >= 8 ){
+		if ($num%2==0 || $num%3==0 || $num%5==0) {
+		return false;
+		}
+		}
+if($num >= 8 ){
+		if ($num%2>=1 || $num%3>=1 || $num%5>=1) {
+		return true;
+		}
+}
+}
+?>
